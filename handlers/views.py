609808c065e32fe1cdb3138e014e1846d8d5c706
shanyu-e <554816284@qq.com>
2016-05-25 13:58:38 +0800

    开始写上传

--- handlers/views.py
+++ handlers/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import jsonify, request, render_template
+import hashlib
+from flask import jsonify, request, render_template, session
+import flask
 from handlers import view
-from utils.models import UserForm
+from mongoengine import DoesNotExist
+from utils.models import UserForm, User
 from utils.util import acquire_admin
 
 
@@ -13,15 +16,29 @@ def login():
 
 @view.route('/login/validate', methods=['POST'])
 def login_validate():
-    form = UserForm(request.form)
-    print dir(form.username), dir(form.password_hash)
-    return
-    # return render_template('login.html', form=form)
+    m = hashlib.md5()
+    user = User()
+    form = UserForm(request.form, obj=user)
+    form.populate_obj(user)
+    m.update(user.password_hash)
+    c_password = m.hexdigest()
+    try:
+        c_user = User.objects.get(username=user.username)
+    except DoesNotExist:
+        flask.abort(403)
+    if c_user.password_hash == c_password:
+        session['user'] = c_user.username
+        session['role'] = c_user.role
+    return flask.redirect(flask.url_for('view.upload'))
 
 
 @view.route('/upload')
 @acquire_admin
 def upload():
-    userid = request.args.get('userid', '')
-    print('xxx', userid)
+    try:
+        user = User.objects.get(username=session['user'])
+    except DoesNotExist:
+        flask.abort(403)
+    print('xxx', user)
+    # session.pop('role')
     return jsonify({"aa": "bb"})
