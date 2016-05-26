08e47f34f6e53ac1ffb3a2848020b1bc05fe1a27
shanyu-e <554816284@qq.com>
2016-05-26 15:49:17 +0800

    错误师范

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
+from conf import FLASK_MONGO_ENGINE_CONF
 from flask import Blueprint
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from utils.models import engine
 
 
 app = Flask(__name__)
+app.config.update(FLASK_MONGO_ENGINE_CONF)
+engine.init_app(app)
+bootstrap = Bootstrap(app)
+
+app.use_api = True
+# app.use_api = False
+
 
 api = Blueprint('api', __name__)
 user = Blueprint('user', __name__)
 other = Blueprint('other', __name__)
 view = Blueprint('view', __name__)
+
+
+
+
+app.register_blueprint(api)
+app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(other, url_prefix='/other')
+# 后面非API
+app.register_blueprint(view)
+
 
 import users
 import others
--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 # coding=utf-8
-from conf import INNER_IP, port, FLASK_MONGO_ENGINE_CONF
-from flask.ext.bootstrap import Bootstrap
+from conf import INNER_IP, port
 from handlers import user, other, api, view, app
-from utils.models import engine
-
-
-app.config.update(FLASK_MONGO_ENGINE_CONF)
-engine.init_app(app)
-bootstrap = Bootstrap(app)
-
-app.use_api = True
-# app.use_api = False
 
 
 if __name__ == '__main__':
-    app.register_blueprint(api)
-    app.register_blueprint(user, url_prefix='/user')
-    app.register_blueprint(other, url_prefix='/other')
-    # 后面非API
-    app.register_blueprint(view)
+
 
     app.run(debug=True, host=INNER_IP, port=port)
