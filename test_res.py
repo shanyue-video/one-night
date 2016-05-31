797891387144310a0b9f0ea28b2621bf2dd24019
shanyu-e <554816284@qq.com>
2016-05-31 17:34:45 +0800

    格式化 输出

--- test_res.py
+++ test_res.py
@@ -1,43 +1,29 @@
 # coding=utf-8
 from utils.conf import port
 
-__author__ = "dengjing"
-
 
 task1 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
-    "courses": "场景设计",  # 课程名称
+    "data": []
 }
 
 task2 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
-    "data": [
-        {
-            "courseId": "12",  # 课程id
-            "courseName": "场景设计",  # 课程名称
-            "courseImg": "",  # 课程图片
-            "browse": "",  # 浏览量
-            "downNum": "",  # 下载量
-            "courseType": "",  # 课程类型
-        },
-        {
-            "courseId": "12",  # 课程id
-            "courseName": "场景设计",  # 课程名称
-            "courseImg": "",  # 课程图片
-            "browse": "",  # 浏览量
-            "downNum": "",  # 下载量
-            "courseType": "",  # 课程类型
-        }
-    ]
+    "data": []
 }
 
 task3 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
-    "classId": "12",  # 分类id
-    "className": "场景设计",  # 分类名称
+    "data": [
+        {
+            "classId": "12",  # 分类id
+            "className": "场景设计",  # 分类名称
+        }
+    ]
+
 }
 
 task4 = {
@@ -55,65 +41,76 @@ task4 = {
 task5 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
+    "data": []
 }
 
 task6 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
-    "courseId": "12",  # 课程id
-    "courseName": "场景设计",  # 课程名称
-    "courseImg": "",  # 课程图片
-    "lecturerId": "",  # 课程讲师名称
-    "lecturerName": "",  # 课程讲师名称
-    "courseIntroduce": "",  # 课程介绍
-    "browse": "",  # 浏览量
-    "isCollection": "",  # 是否收藏
-    "isDownload": "",  # 是否下载
-    "commentList": [
+    "data": [
         {
-            "commentId": "12",  # 评论id
-            "commentContent": "",  # 评论内容
-            "userId": "",  # 评论人
-            "userName": "",  # 评论名字
-            "commentTime": "",  # 评论时间
-        },
-        {
-            "commentId": "12",  # 评论id
-            "commentContent": "",  # 评论内容
-            "userId": "",  # 评论人
-            "userName": "",  # 评论名字
-            "commentTime": "",  # 评论时间
-        },
-    ],
-    "chapter": [
-        {
-            "chapterId": "12",  # 章节id
-            "chapterName": "场景设计",  # 章节名称
-            "chapterUrl": "",  # 章节视频地址
-            "chapterime": "",  # 章节时长
-        },
-        {
-            "chapterId": "12",  # 章节id
-            "chapterName": "场景设计",  # 章节名称
-            "chapterUrl": "",  # 章节视频地址
-            "chapterime": "",  # 章节时长
+            "courseId": "12",  # 课程id
+            "courseName": "场景设计",  # 课程名称
+            "courseImg": "",  # 课程图片
+            "lecturerId": "",  # 课程讲师名称
+            "lecturerName": "",  # 课程讲师名称
+            "courseIntroduce": "",  # 课程介绍
+            "browse": "",  # 浏览量
+            "isCollection": "",  # 是否收藏
+            "isDownload": "",  # 是否下载
+            "commentList": [
+                {
+                    "commentId": "12",  # 评论id
+                    "commentContent": "",  # 评论内容
+                    "userId": "",  # 评论人
+                    "userName": "",  # 评论名字
+                    "commentTime": "",  # 评论时间
+                },
+                {
+                    "commentId": "12",  # 评论id
+                    "commentContent": "",  # 评论内容
+                    "userId": "",  # 评论人
+                    "userName": "",  # 评论名字
+                    "commentTime": "",  # 评论时间
+                },
+            ],
+            "chapter": [
+                {
+                    "chapterId": "12",  # 章节id
+                    "chapterName": "场景设计",  # 章节名称
+                    "chapterUrl": "",  # 章节视频地址
+                    "chapterime": "",  # 章节时长
+                },
+                {
+                    "chapterId": "12",  # 章节id
+                    "chapterName": "场景设计",  # 章节名称
+                    "chapterUrl": "",  # 章节视频地址
+                    "chapterime": "",  # 章节时长
+                }
+            ]
         }
     ]
+
 }
 
 task7 = {
     "status": 1,  # 状态标识
     "info": u"正常",  # 状态说明
-    "chapter": {
-        "chapterId": "12",  # 章节id
-        "chapterName": "场景设计",  # 章节名称
-        "chapterDownUrl": ""  # 章节视频地址
-    }
+    "data": [
+        {
+            "chapter": {
+                "chapterId": "12",  # 章节id
+                "chapterName": "场景设计",  # 章节名称
+                "chapterDownUrl": ""  # 章节视频地址
+            }
+        }
+    ]
 }
 
 task8 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
+    "data": []
 }
 
 task9 = {
@@ -203,113 +200,129 @@ task11 = {
 task12 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
+    "data": []
 }
 
 task13 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
-    "post": {
-        "postId": "",  # 帖子id
-        "userId": "",  # 发帖人id
-        "postName": "",  # 帖子name
-        "userName": "",  # 发帖人姓名
-        "userImg": "",  # 发帖人头像
-        "postTime": "",  # 发帖时间
-        "postContent": "",  # 发帖内容
-        "postImg": "",  # 发帖图片多张图片以分号分隔
-        "likeNum": "12",  # 点赞数
-        "browseNum": "16",  # 浏览量
-        "commentNum": "20",  # 评论数
-        "commentList": [
-            {
-                "commentId": "12",  # 评论id
-                "commentContent": "",  # 评论内容
-                "userId": "",  # 评论人
-                "userName": "",  # 评论名字
-                "userImg": "",  # 评论头像
-                "commentTime": "",  # 评论时间
-                "userType": "",  # 评论人身份
-            },
-            {
-                "commentId": "12",  # 评论id
-                "commentContent": "",  # 评论内容
-                "userId": "",  # 评论人
-                "userName": "",  # 评论名字
-                "userImg": "",  # 评论头像
-                "commentTime": "",  # 评论时间
-                "userType": "",  # 评论人身份
-                "commentImg": "",  # 评论图片
-                "commentVoice": "",  # 评论语音
+    "data": [
+        {
+            "post": {
+                "postId": "",  # 帖子id
+                "userId": "",  # 发帖人id
+                "postName": "",  # 帖子name
+                "userName": "",  # 发帖人姓名
+                "userImg": "",  # 发帖人头像
+                "postTime": "",  # 发帖时间
+                "postContent": "",  # 发帖内容
+                "postImg": "",  # 发帖图片多张图片以分号分隔
+                "likeNum": "12",  # 点赞数
+                "browseNum": "16",  # 浏览量
+                "commentNum": "20",  # 评论数
+                "commentList": [
+                    {
+                        "commentId": "12",  # 评论id
+                        "commentContent": "",  # 评论内容
+                        "userId": "",  # 评论人
+                        "userName": "",  # 评论名字
+                        "userImg": "",  # 评论头像
+                        "commentTime": "",  # 评论时间
+                        "userType": "",  # 评论人身份
+                    },
+                    {
+                        "commentId": "12",  # 评论id
+                        "commentContent": "",  # 评论内容
+                        "userId": "",  # 评论人
+                        "userName": "",  # 评论名字
+                        "userImg": "",  # 评论头像
+                        "commentTime": "",  # 评论时间
+                        "userType": "",  # 评论人身份
+                        "commentImg": "",  # 评论图片
+                        "commentVoice": "",  # 评论语音
+                    }
+                ]
             }
-        ]
-    }
+        }
+    ]
+
 }
 
 task14 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
-    "post": {
-        "postId": "",  # 帖子id
-        "userId": "",  # 发帖人id
-        "postName": "",  # 帖子name
-        "userName": "",  # 发帖人姓名
-        "userImg": "",  # 发帖人头像
-        "postTime": "",  # 发帖时间
-        "postContent": "",  # 发帖内容
-        "postImg": "",  # 发帖图片多张图片以分号分隔
-        "likeNum": "12",  # 点赞数
-        "browseNum": "16",  # 浏览量
-        "commentNum": "20",  # 评论数
-        "commentList": [
-            {
-                "commentId": "12",  # 评论id
-                "commentContent": "",  # 评论内容
-                "userId": "",  # 评论人
-                "userName": "",  # 评论名字
-                "userImg": "",  # 评论头像
-                "commentTime": "",  # 评论时间
-                "userType": "",  # 评论人身份
-            },
-            {
-                "commentId": "12",  # 评论id
-                "commentContent": "",  # 评论内容
-                "userId": "",  # 评论人
-                "userName": "",  # 评论名字
-                "userImg": "",  # 评论头像
-                "commentTime": "",  # 评论时间
-                "userType": "",  # 评论人身份
-                "commentImg": "",  # 评论图片
-                "commentVoice": "",  # 评论语音
+    "data": [
+        {
+            "post": {
+                "postId": "",  # 帖子id
+                "userId": "",  # 发帖人id
+                "postName": "",  # 帖子name
+                "userName": "",  # 发帖人姓名
+                "userImg": "",  # 发帖人头像
+                "postTime": "",  # 发帖时间
+                "postContent": "",  # 发帖内容
+                "postImg": "",  # 发帖图片多张图片以分号分隔
+                "likeNum": "12",  # 点赞数
+                "browseNum": "16",  # 浏览量
+                "commentNum": "20",  # 评论数
+                "commentList": [
+                    {
+                        "commentId": "12",  # 评论id
+                        "commentContent": "",  # 评论内容
+                        "userId": "",  # 评论人
+                        "userName": "",  # 评论名字
+                        "userImg": "",  # 评论头像
+                        "commentTime": "",  # 评论时间
+                        "userType": "",  # 评论人身份
+                    },
+                    {
+                        "commentId": "12",  # 评论id
+                        "commentContent": "",  # 评论内容
+                        "userId": "",  # 评论人
+                        "userName": "",  # 评论名字
+                        "userImg": "",  # 评论头像
+                        "commentTime": "",  # 评论时间
+                        "userType": "",  # 评论人身份
+                        "commentImg": "",  # 评论图片
+                        "commentVoice": "",  # 评论语音
+                    }
+                ]
             }
-        ]
-    }
+        }
+    ]
 }
 
 task15 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
+    "data": []
 }
 
 task16 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
-    " userId": "",  # 用户id
-    "userName": "",  # 发帖人name
-    "userType": "",  # 用户类型
-    "birthday": "2015-09-10 00:00:00",
-    "sex": "1",
-    "mobile": "1354231321",
-    "email": "",
-    "iconUrl": "",  # 头像
-    "nickName": "",  # 昵称
-    "accessToken": "",  # 授权码
-    "studyTime": "",  # 学习时间
-    "ranking": ""  # 学习排名
+    "data": [
+        {
+            "userId": "",  # 用户id
+            "userName": "",  # 发帖人name
+            "userType": "",  # 用户类型
+            "birthday": "2015-09-10 00:00:00",
+            "sex": "1",
+            "mobile": "1354231321",
+            "email": "",
+            "iconUrl": "",  # 头像
+            "nickName": "",  # 昵称
+            "accessToken": "",  # 授权码
+            "studyTime": "",  # 学习时间
+            "ranking": ""  # 学习排名
+        }
+    ]
 }
 
 task17 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
+    "data": []
 }
 
 task18 = {
@@ -337,18 +350,22 @@ task18 = {
 task19 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
     "info": u"正常",  # 状态说明
-    "userid": "",  # 用户id
-    "ranking": "",  # 名次
-    "iconUrl": "",  # 头像
-    "nickname": "",  # 呢称
-    "studyTime": "",  # 学习时长
-    "isZan": "",  # 是否点赞
     "data": [
         {
+            "userid": "",  # 用户id
+            "ranking": "",  # 名次
+            "iconUrl": "",  # 头像
+            "nickname": "",  # 呢称
+            "isZan": "",  # 是否点赞
             "datetime": "2016-05-14",  # 哪一天
             "studyTime": "300",  # 学习时间 单位为分钟
         },
         {
+            "userid": "",  # 用户id
+            "ranking": "",  # 名次
+            "iconUrl": "",  # 头像
+            "nickname": "",  # 呢称
+            "isZan": "",  # 是否点赞
             "datetime": "2016-05-15",  # 哪一天
             "studyTime": "300",  # 学习时间 单位为分钟
         }
@@ -423,7 +440,8 @@ task21 = {
 
 task22 = {
     "status": 1,  # 状态标识 1表示成功，0表示失败
-    "info": u"正常", 	# 状态说明
+    "info": u"正常",  # 状态说明
+    "data": []
 }
 
 # port = '8888'
@@ -543,7 +561,6 @@ task23 = {
         },
     ]
 }
-
 
 if __name__ == '__main__':
     print len(task23['data'])
