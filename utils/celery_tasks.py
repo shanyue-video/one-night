b2d652f22008f326a15ee39eb23fea2ebc969ea2
shanyu-e <554816284@qq.com>
2016-07-22 11:30:00 +0800

    ok 配置 celery

---
+++ utils/celery_tasks.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from celery import Celery
+from utils.conf import FLASK_MONGO_ENGINE_CONF
+
+MONGODB = FLASK_MONGO_ENGINE_CONF.get('MONGODB_SETTINGS')
+
+app = Celery(broker=MONGODB.get('host'))
+app.conf.update(
+    CELERY_ACCEPT_CONTENT=['pickle', 'json', 'msgpack', 'yaml'],
+    CELERY_RESULT_BACKEND=MONGODB.get('host'),
+    CELERY_MONGODB_BACKEND_SETTINGS={
+        'database': 'one_night_test',
+        'taskmeta_collection': 'one_night_test',
+    },
+)
