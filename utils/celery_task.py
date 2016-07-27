b61c517aeb2f47817fd18442ebad018714121a78
shanyu-e <554816284@qq.com>
2016-07-27 16:40:43 +0800

    ...七牛上 试试

--- utils/celery_task.py
+++ utils/celery_task.py
@@ -8,7 +8,7 @@ from utils.conf import UPLOAD_FOLDER
 @app.task(time_limit=180, soft_time_limit=120, acks_late=True)
 def task(url, _id):
     print 'ddd...', url.split('/')[-1], '---', _id
-    filename = os.path.join(UPLOAD_FOLDER, _id+'_tmp')
+    filename = os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp')
     urllib.urlretrieve(url, filename)
     print 'over download %s' % filename
     return 'over...'
