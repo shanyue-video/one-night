608a4221244a6722964ee44dae0f95697b68dca2
shanyu-e <554816284@qq.com>
2016-07-28 14:33:42 +0800

    celery 下载文件 其他进程抢占

--- utils/celery_task.py
+++ utils/celery_task.py
@@ -3,12 +3,15 @@ import os
 import urllib
 from utils.celery_tasks import app
 from utils.conf import UPLOAD_FOLDER
+import shutil
 
 
 @app.task(time_limit=180, soft_time_limit=120, acks_late=True)
 def task(url, _id):
     print 'ddd...', url.split('/')[-1], '---', _id
+    filename_tmp = os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp1')
     filename = os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp')
-    urllib.urlretrieve(url, filename)
-    print 'over download %s' % filename
+    urllib.urlretrieve(url, filename_tmp)
+    print 'over download %s' % filename_tmp
+    shutil.move(filename_tmp, filename)
     return 'over...'
