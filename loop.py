ea82b0bad73f0ece4dc715b06bd3b52ecec379b9
shanyu-e <554816284@qq.com>
2016-08-10 10:53:08 +0800

    ...

--- loop.py
+++ loop.py
@@ -36,7 +36,8 @@ def loop_qiniu():
     while True:
         for f in os.listdir(UPLOAD_FOLDER):
             if not 'tmp' == f.split('_')[-1]:
-                os.remove(os.path.join(UPLOAD_FOLDER, f))
+                # os.remove(os.path.join(UPLOAD_FOLDER, f))
+                pass
             else:
                 print 'start handle %s' % f
                 f_path = os.path.join(UPLOAD_FOLDER, f)
