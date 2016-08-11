7003e628dada07801fd7a7d564e810f109aa1db2
shanyu-e <554816284@qq.com>
2016-08-11 13:56:04 +0800

    ...

--- utils/celery_task.py
+++ utils/celery_task.py
@@ -4,14 +4,13 @@ import urllib
 from utils.celery_tasks import app
 from utils.conf import UPLOAD_FOLDER
 import shutil
-from utils.util import u_path
 
 
 @app.task(acks_late=False)
 def task(url, _id):
     print 'celery start...', url.split('/')[-1], '---', _id
-    filename_tmp = u_path(os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp1'))
-    filename = u_path(os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp'))
+    filename_tmp = os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp1')
+    filename = os.path.join(UPLOAD_FOLDER, _id + url.split('/')[-1] + '_tmp')
     urllib.urlretrieve(url, filename_tmp)
     print 'over download %s' % filename_tmp
     shutil.move(filename_tmp, filename)
