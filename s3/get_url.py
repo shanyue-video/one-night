719de1e6b7073448bac58dd69de864c2f5695696
shanyu-e <554816284@qq.com>
2016-08-12 18:16:01 +0800

    ...

--- s3/get_url.py
+++ s3/get_url.py
@@ -4,7 +4,7 @@ import boto3
 
 Bucket = 'one-night-1'
 # base_url = 'http://o7z6eyjps.bkt.clouddn.com/'
-base_url = 'http://obsib7bzs.bkt.clouddn.com/'
+base_url = 'http://obsk2aox1.bkt.clouddn.com/'
 
 
 def get_url(key):
