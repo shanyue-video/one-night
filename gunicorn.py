7f857b00fdf3ea5986890008628d51cc7b23a781
shanyu-e <554816284@qq.com>
2016-10-29 18:30:38 +0800

    ...

--- gunicorn.py
+++ gunicorn.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 import os
-from utils.conf import INNER_IP, port
+# from utils.conf import INNER_IP, port
 
 __author__ = 'dengjing'
-# bind = '0.0.0.0:8888'  # 绑定的端口
-bind = '%s:%s' % (INNER_IP, port)
+bind = '0.0.0.0:8888'  # 绑定的端口
+# bind = '%s:%s' % (INNER_IP, port)
 workers = 4  # worker数量
 backlog = 2048
 # debug = True
